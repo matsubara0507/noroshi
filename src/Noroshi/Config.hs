2fcb4b50d02f322a0e8a8b845b8ec2fede6377cd
MATSUBARA Nobutada <nobutada.matsubara@mixi.co.jp>
2018-10-27 22:06:08 +0900

    Feat: default config

--- src/Noroshi/Config.hs
+++ src/Noroshi/Config.hs
@@ -1,13 +1,17 @@
 {-# LANGUAGE DataKinds        #-}
 {-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE TemplateHaskell  #-}
 {-# LANGUAGE TypeOperators    #-}
 
 module Noroshi.Config where
 
 import           RIO
+import           RIO.Directory           (doesFileExist)
 
 import           Data.Extensible
 import qualified Data.Yaml               as Y
+import qualified Data.Yaml.TH            as YTH
+import           Instances.TH.Lift       ()
 import           Noroshi.Data.ConfigInfo (ConfigInfo)
 
 type Config = Record
@@ -15,5 +19,25 @@ type Config = Record
    , "configs" >: [ConfigInfo]
    ]
 
+defaultConfig :: Config
+defaultConfig = $$(YTH.decodeFile "template/config.yaml")
+
 readConfig :: (MonadIO m, MonadThrow m) => FilePath -> m Config
-readConfig = Y.decodeFileThrow
+readConfig = readConfigWith defaultConfig
+
+readConfigWith :: (MonadIO m, MonadThrow m) => Config -> FilePath -> m Config
+readConfigWith def path = do
+  file <- readFileBinaryWith "" path
+  case Y.decodeEither' file of
+    Right Y.Null -> pure def
+    _ -> do
+      config <- either throwM pure $ Y.decodeEither' file
+      pure $ constructWith def config
+
+readFileBinaryWith :: MonadIO m => ByteString -> FilePath -> m ByteString
+readFileBinaryWith def path =
+  doesFileExist path >>= bool (pure def) (readFileBinary path)
+
+constructWith :: RecordOf h xs -> Nullable (Field h) :* xs -> RecordOf h xs
+constructWith def =
+  hmapWithIndex $ \m x -> fromMaybe (hlookup m def) (getNullable x)
